b86185a07bcef22955b6cdfc6fda7270f2283d8b
Saman Tofighian <96416020+saman-tofighian@users.noreply.github.com>
2025-03-03 22:58:36 +0330

    Created by Saman Tofighian

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import Table from "./Components/Model/Table";
+import { useState } from "react";
+
+function App() {
+  const [colorTable, setColorTable] = useState("#fff");
+  const [colorChair, setColorChair] = useState("#fff");
+  const [colorPlate, setColorPlate] = useState("#fff");
+
+  const [data] = useState({
+    title: {
+      t1: "Change Table Color : ",
+      t2: "Change Chair Color : ",
+      t3: "Change Plate Color : ",
+    },
+  });
+  return (
+    <div id="App">
+      <div className="grid grid-cols-12 h-[100vh]">
+        <Canvas dpr={[1, 2]} className="col-span-9 xl:col-span-10">
+          <Table
+            colorTable={colorTable}
+            colorChair={colorChair}
+            colorPlate={colorPlate}
+          />
+        </Canvas>
+        <div className="col-span-3 xl:col-span-2 py-8  gap-5" id="controller">
+          <div className="w-full">
+            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+              {data.title.t1}{" "}
+            </h5>
+            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer "
+                onClick={() => setColorTable("#8B5A2B")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#F5F5F5] cursor-pointer "
+                onClick={() => setColorTable("#F5F5F5")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#1A1A1A] cursor-pointer "
+                onClick={() => setColorTable("#1A1A1A")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#556B2F] cursor-pointer"
+                onClick={() => setColorTable("#556B2F")}
+              ></button>
+            </div>
+          </div>
+
+          <div className="w-full mt-[2rem] xl:mt-[5rem]">
+            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+              {data.title.t2}
+            </h5>
+            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer"
+                onClick={() => setColorChair("#8B5A2B")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#F5F5F5] cursor-pointer"
+                onClick={() => setColorChair("#F5F5F5")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#1A1A1A] cursor-pointer"
+                onClick={() => setColorChair("#1A1A1A")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#556B2F] cursor-pointer"
+                onClick={() => setColorChair("#556B2F")}
+              ></button>
+            </div>
+          </div>
+
+          <div className="w-full mt-[2rem] xl:mt-[5rem] ">
+            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+              {data.title.t3}
+            </h5>
+            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer"
+                onClick={() => setColorPlate("#8B5A2B")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#F5F5F5] cursor-pointer"
+                onClick={() => setColorPlate("#F5F5F5")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#1A1A1A] cursor-pointer"
+                onClick={() => setColorPlate("#1A1A1A")}
+              ></button>
+              <button
+                className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#556B2F] cursor-pointer"
+                onClick={() => setColorPlate("#556B2F")}
+              ></button>
+            </div>
+          </div>
+
+          <div className="w-full mt-[2rem] xl:mt-[5rem] px-2 justify-center flex">
+            <button className="py-3 px-2 xl:py-3 xl:px-7 rounded-2xl text-white bg-green-700 text-[12px] xl:text-[16px]">
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Model/Table.jsx
@@ -0,0 +1,85 @@
+import {
+  PresentationControls,
+  Stage,
+  useGLTF,
+  useTexture,
+} from "@react-three/drei";
+import gsap from "gsap";
+import * as THREE from "three";
+import { useEffect } from "react";
+
+export default function Table({
+  colorTable,
+  colorChair,
+  colorPlate,
+  textureTable,
+}) {
+  const model = useGLTF("./dining_scene.glb");
+
+  model.scene.rotation.y = Math.PI / 2;
+
+  const tx = useTexture("./map/1.jpg");
+
+  model.scene.children[0].children[0].children[0].children[1].children[0].material.map =
+    tx;
+  model.scene.children[0].children[0].children[0].children[0].children[0].material.map =
+    tx;
+
+  let chairMaterial =
+    model.scene.children[0].children[0].children[0].children[0].children[0]
+      .material;
+  const newColor = new THREE.Color(colorChair);
+
+  useEffect(() => {
+    gsap.to(chairMaterial.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+      duration: 2,
+      ease: "linear",
+    });
+  }, [colorChair]);
+
+  let tableMaterial =
+    model.scene.children[0].children[0].children[0].children[1].children[0]
+      .material;
+  const newColorTable = new THREE.Color(colorTable);
+
+  useEffect(() => {
+    gsap.to(tableMaterial.color, {
+      r: newColorTable.r,
+      g: newColorTable.g,
+      b: newColorTable.b,
+      duration: 2,
+      ease: "linear",
+    });
+  }, [colorTable]);
+
+  let plateMaterial =
+    model.scene.children[0].children[0].children[0].children[22].children[0]
+      .material;
+  const newColorPlate = new THREE.Color(colorPlate);
+
+  useEffect(() => {
+    gsap.to(plateMaterial.color, {
+      r: newColorPlate.r,
+      g: newColorPlate.g,
+      b: newColorPlate.b,
+      duration: 2,
+      ease: "linear",
+    });
+  }, [colorPlate]);
+
+  return (
+    <PresentationControls
+      speed={1.5}
+      global
+      zoom={0.7}
+      polar={[-0.1, Math.PI / 4]}
+    >
+      <Stage environment="city" intensity={11} shadows={null}>
+        <primitive object={model.scene} />
+      </Stage>
+    </PresentationControls>
+  );
+}

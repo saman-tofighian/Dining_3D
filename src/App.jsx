81124335dfc6cbe761c4d98eeb815e2065e69faa
Saman Tofighian <96416020+saman-tofighian@users.noreply.github.com>
2025-03-04 10:10:01 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Table from "./Components/Model/Table";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 function App() {
   const [colorTable, setColorTable] = useState("#fff");
@@ -15,6 +16,15 @@ function App() {
       t3: "Change Plate Color : ",
     },
   });
+
+  const showAlert = () => {
+    Swal.fire({
+      title: "Your custom dining added to your cart",
+      icon: "success",
+      draggable: false,
+    });
+  };
+
   return (
     <div id="App">
       <div className="grid grid-cols-12 h-[100vh]">
@@ -25,12 +35,12 @@ function App() {
             colorPlate={colorPlate}
           />
         </Canvas>
-        <div className="col-span-3 xl:col-span-2 py-8  gap-5" id="controller">
+        <div className="col-span-3 gap-5 py-8 xl:col-span-2" id="controller">
           <div className="w-full">
-            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+            <h5 className="text-[11px] xl:text-[16px] text-white ps-2">
               {data.title.t1}{" "}
             </h5>
-            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+            <div className="flex flex-wrap justify-center w-full mt-6 gap-y-5 gap-x-3">
               <button
                 className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer "
                 onClick={() => setColorTable("#8B5A2B")}
@@ -51,10 +61,10 @@ function App() {
           </div>
 
           <div className="w-full mt-[2rem] xl:mt-[5rem]">
-            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+            <h5 className="text-[11px] xl:text-[16px] text-white ps-2">
               {data.title.t2}
             </h5>
-            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+            <div className="flex flex-wrap justify-center w-full mt-6 gap-y-5 gap-x-3">
               <button
                 className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer"
                 onClick={() => setColorChair("#8B5A2B")}
@@ -75,10 +85,10 @@ function App() {
           </div>
 
           <div className="w-full mt-[2rem] xl:mt-[5rem] ">
-            <h5 className="text-[12px] xl:text-[16px] text-white ps-2">
+            <h5 className="text-[11px] xl:text-[16px] text-white ps-2">
               {data.title.t3}
             </h5>
-            <div className="w-full flex justify-center mt-6 flex-wrap gap-y-5 gap-x-3">
+            <div className="flex flex-wrap justify-center w-full mt-6 gap-y-5 gap-x-3">
               <button
                 className="w-[35px] h-[35px] xl:w-[40px] xl:h-[40px] rounded-[50%] bg-[#8B5A2B] cursor-pointer"
                 onClick={() => setColorPlate("#8B5A2B")}
@@ -99,7 +109,10 @@ function App() {
           </div>
 
           <div className="w-full mt-[2rem] xl:mt-[5rem] px-2 justify-center flex">
-            <button className="py-3 px-2 xl:py-3 xl:px-7 rounded-2xl text-white bg-green-700 text-[12px] xl:text-[16px]">
+            <button
+              className="py-3 px-2 xl:py-3 xl:px-7 rounded-2xl text-white bg-green-700 text-[12px] xl:text-[16px]"
+              onClick={showAlert}
+            >
               Add To Cart
             </button>
           </div>
